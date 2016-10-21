c3d6aa1f6a165ea764426f72d190a0e30a5f9b82
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2016-10-21 11:06:39 +0800

    Added If only I can Say I miss and I love you

--- Assets/Scripts/DragonKnightScript/DragonKnight.cs
+++ Assets/Scripts/DragonKnightScript/DragonKnight.cs
@@ -5,7 +5,7 @@ public class DragonKnight : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print ("If only I can say I miss you and I love you");
 	}
 	
 	// Update is called once per frame
